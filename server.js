f3f123e839a4500ec6d6a8094210823f176e029f
SerhiiKutovyi <wagenrex@gmail.com>
2023-03-23 19:53:40 +0200

    mongoose start

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
-app.listen(3001, () => {
-  console.log('Server running. Use our API on port: 3001');
-});
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3001))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
