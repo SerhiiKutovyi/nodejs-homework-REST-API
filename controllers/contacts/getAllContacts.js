73fec6a82f0ddd887144c228c855a86c7671a5a9
SerhiiKutovyi <wagenrex@gmail.com>
2023-04-09 02:34:56 +0300

    add pagination

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -2,10 +2,12 @@ const { Contact } = require('../../models/contact');
 
 const getAllContacts = async (req, res) => {
   const { _id } = req.user;
-  const data = await Contact.find({ owner: _id }).populate(
-    'owner',
-    '_id email'
-  );
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const data = await Contact.find({ owner: _id }, '', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', '_id email');
   res.json(data);
 };
 
